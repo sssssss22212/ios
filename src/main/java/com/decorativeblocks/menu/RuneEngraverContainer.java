d898aa8410060a252bcb851494ef6ceb8e5de1ff
sssssss22212 <maxttoper@gmail.com>
2026-03-07 11:58:59 +0300

    fix imports

--- src/main/java/com/decorativeblocks/menu/RuneEngraverContainer.java
+++ src/main/java/com/decorativeblocks/menu/RuneEngraverContainer.java
@@ -1,7 +1,5 @@
 package com.decorativeblocks.menu;
 
-import com.decorativeblocks.init.ModMenuTypes;
-import com.decorativeblocks.init.ModItems;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventory;
@@ -11,55 +9,26 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketBuffer;
 
 public class RuneEngraverContainer extends Container {
-    private final Inventory internal = new Inventory(5); // 1 item + 3 runes + 1 output
+    private final Inventory internal = new Inventory(5);
 
-    public RuneEngraverContainer(int windowId, PlayerInventory playerInv, PacketBuffer data) {
-        this(windowId, playerInv);
+    public RuneEngraverContainer(int id, PlayerInventory inv, PacketBuffer buf) { this(id, inv); }
+
+    public RuneEngraverContainer(int id, PlayerInventory inv) {
+        super(ModMenuTypes.RUNE_ENGRAVER.get(), id);
+        addSlot(new Slot(internal, 0, 26, 35));
+        addSlot(new Slot(internal, 1, 62, 17));
+        addSlot(new Slot(internal, 2, 80, 17));
+        addSlot(new Slot(internal, 3, 98, 17));
+        addSlot(new Slot(internal, 4, 134, 35) {
+            @Override public boolean mayPlace(ItemStack s) { return false; }
+        });
+        for (int r = 0; r < 3; r++)
+            for (int c = 0; c < 9; c++)
+                addSlot(new Slot(inv, c+r*9+9, 8+c*18, 112+r*18));
+        for (int c = 0; c < 9; c++)
+            addSlot(new Slot(inv, c, 8+c*18, 170));
     }
 
-    public RuneEngraverContainer(int windowId, PlayerInventory playerInv) {
-        super(ModMenuTypes.RUNE_ENGRAVER.get(), windowId);
-
-        // Item to engrave
-        this.addSlot(new Slot(internal, 0, 26, 35));
-        // 3 rune slots
-        this.addSlot(new Slot(internal, 1, 62, 17) {
-            @Override public boolean mayPlace(ItemStack stack) {
-                return stack.getItem() == ModItems.ANCIENT_FRAGMENT.get()
-                    || stack.getItem() == ModItems.RUBY.get()
-                    || stack.getItem() == ModItems.SAPPHIRE.get()
-                    || stack.getItem() == ModItems.AMETHYST_GEM.get();
-            }
-        });
-        this.addSlot(new Slot(internal, 2, 80, 17) {
-            @Override public boolean mayPlace(ItemStack stack) {
-                return stack.getItem() == ModItems.ANCIENT_FRAGMENT.get()
-                    || stack.getItem() == ModItems.RUBY.get()
-                    || stack.getItem() == ModItems.SAPPHIRE.get()
-                    || stack.getItem() == ModItems.AMETHYST_GEM.get();
-            }
-        });
-        this.addSlot(new Slot(internal, 3, 98, 17) {
-            @Override public boolean mayPlace(ItemStack stack) {
-                return stack.getItem() == ModItems.ANCIENT_FRAGMENT.get()
-                    || stack.getItem() == ModItems.RUBY.get()
-                    || stack.getItem() == ModItems.SAPPHIRE.get()
-                    || stack.getItem() == ModItems.AMETHYST_GEM.get();
-            }
-        });
-        // Output
-        this.addSlot(new Slot(internal, 4, 134, 35) {
-            @Override public boolean mayPlace(ItemStack stack) { return false; }
-        });
-
-        // Player inventory
-        for (int row = 0; row < 3; row++)
-            for (int col = 0; col < 9; col++)
-                this.addSlot(new Slot(playerInv, col + row * 9 + 9, 8 + col * 18, 112 + row * 18));
-        for (int col = 0; col < 9; col++)
-            this.addSlot(new Slot(playerInv, col, 8 + col * 18, 170));
-    }
-
-    @Override public boolean stillValid(PlayerEntity player) { return true; }
-    @Override public ItemStack quickMoveStack(PlayerEntity player, int index) { return ItemStack.EMPTY; }
+    @Override public boolean stillValid(PlayerEntity p) { return true; }
+    @Override public ItemStack quickMoveStack(PlayerEntity p, int i) { return ItemStack.EMPTY; }
 }
